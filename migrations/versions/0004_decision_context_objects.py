e5d4414f6a9ddd04a45b490627ef2f37a3e8beaf
Kaspar Brönnimann <k.broennimann@gmail.com>
2026-05-18 11:13:19 +0200

    Fix decision context migration head

--- migrations/versions/0004_decision_context_objects.py
+++ migrations/versions/0004_decision_context_objects.py
@@ -8,7 +8,7 @@ from alembic import op
 import sqlalchemy as sa
 
 revision = "0004_decision_context_objects"
-down_revision = "0003_merge_alembic_heads"
+down_revision = "31307c981c0a"
 branch_labels = None
 depends_on = None
 
